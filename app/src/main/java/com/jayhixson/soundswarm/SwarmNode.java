a4e35067f832c9afebbe4b99eccdd27a81ee6309
subliminalj <jshtz8@mail.umkc.edu>
2018-02-14 16:35:25 -0600

    Created SwarmNode class with getters and setters

---
+++ app/src/main/java/com/jayhixson/soundswarm/SwarmNode.java
@@ -0,0 +1,83 @@
+package com.jayhixson.soundswarm;
+
+import java.util.UUID;
+
+/**
+ * Created by jayhixson on 2/14/18.
+ * A SwarmNode is a an audiofile unit with properties to identify it and for playback.
+ */
+
+public class SwarmNode {
+    private UUID mId;
+    private String mFileName;
+    private String mTitle;
+    private String mDesc;
+    private boolean mLoop;
+    private Float mBegin;
+    private Float mEnd;
+
+    public SwarmNode() {
+        mId = UUID.randomUUID();
+        mFileName = null;
+        mTitle = "Title";
+        mDesc = "Description";
+        mLoop = false;
+        /*
+        mBegin = 0.0;
+        mEnd = file.getLength();
+         */
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getFileName() {
+
+        return mFileName;
+    }
+
+    public void setFileName(String fileName) {
+        mFileName = fileName;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getDesc() {
+        return mDesc;
+    }
+
+    public void setDesc(String desc) {
+        mDesc = desc;
+    }
+
+    public boolean isLoop() {
+        return mLoop;
+    }
+
+    public void setLoop(boolean loop) {
+        mLoop = loop;
+    }
+
+    public Float getBegin() {
+        return mBegin;
+    }
+
+    public void setBegin(Float begin) {
+        mBegin = begin;
+    }
+
+    public Float getEnd() {
+        return mEnd;
+    }
+
+    public void setEnd(Float end) {
+        mEnd = end;
+    }
+}
