6f76a430472c8c37fe43bde2029c470239755087
subliminalj <jshtz8@mail.umkc.edu>
2018-02-26 20:44:41 -0600

    fixed fragment removed toolbar

--- app/src/main/java/com/jayhixson/soundswarm/SwarmFragment.java
+++ app/src/main/java/com/jayhixson/soundswarm/SwarmFragment.java
@@ -1,3 +1,5 @@
+package com.jayhixson.soundswarm;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
--- app/src/main/java/com/jayhixson/soundswarm/SwarmSelector.java
+++ app/src/main/java/com/jayhixson/soundswarm/SwarmSelector.java
@@ -3,6 +3,8 @@ package com.jayhixson.soundswarm;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,6 +17,15 @@ public class SwarmSelector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swarm_selector);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new SwarmFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+
         }
+    }
 
 }
