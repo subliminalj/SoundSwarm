bb87158c7c36c8eafd1aa0248bf23ef892e28295
subliminalj <jshtz8@mail.umkc.edu>
2018-04-18 17:53:23 -0500

    adding mediaplayer and audiofiles

--- app/src/main/java/com/jayhixson/soundswarm/SwarmListFragment.java
+++ app/src/main/java/com/jayhixson/soundswarm/SwarmListFragment.java
@@ -1,6 +1,7 @@
 package com.jayhixson.soundswarm;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
